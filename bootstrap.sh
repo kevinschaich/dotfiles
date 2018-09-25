ea97ee60abb391509ec9310f8d2acd70d245819a
Kevin Schaich <kschaich@palantir.com>
2018-09-24 21:46:28 -0700

    🔥

--- bootstrap.sh
+++ bootstrap.sh
@@ -58,6 +58,8 @@ brew cask install spotify
 brew cask install caprine
 brew cask install kap
 brew cask install alfred
+brew cask install slack
+brew cask install quip
 
 echo "Installing terminal utilities..."
 brew install wget
@@ -69,54 +71,23 @@ brew install node
 brew install yarn
 brew install tldr
 brew install mas
+brew install nativefier
+brew install ag
 npm install --global pure-prompt
 
 echo "Installing Fonts..."
 brew tap caskroom/fonts
 brew cask install font-hack
-brew cask install font-fontawesome
-brew cask install font-alegreya-sans-sc
-brew cask install font-alegreya-sans
-brew cask install font-alegreya-sc
-brew cask install font-alegreya
 brew cask install font-bebas-neue
 brew cask install font-cardo
 brew cask install font-charter
 brew cask install font-code
 brew cask install font-crimson-text
-brew cask install font-droid-sans
-brew cask install font-droid-sans-mono
-brew cask install font-droid-serif
 brew cask install font-fira-code
-brew cask install font-fira-mono
-brew cask install font-fira-sans
-brew cask install font-fjord
 brew cask install font-inconsolata
-brew cask install font-josefin-sans
-brew cask install font-josefin-slab
 brew cask install font-lato
-brew cask install font-league-gothic
-brew cask install font-league-script
-brew cask install font-league-spartan
-brew cask install font-liberation-mono-for-powerline
-brew cask install font-liberation-sans
-brew cask install font-linden-hill
-brew cask install font-lobster-two
-brew cask install font-lora
-brew cask install font-merriweather
-brew cask install font-merriweather-sans
-brew cask install font-montserrat
-brew cask install font-nexa
 brew cask install font-open-sans
 brew cask install font-open-sans-condensed
-brew cask install font-oswald
-brew cask install font-playfair-display
-brew cask install font-playfair-display-sc
-brew cask install font-pt-mono
-brew cask install font-pt-sans
-brew cask install font-pt-serif
-brew cask install font-quicksand
-brew cask install font-raleway
 brew cask install font-roboto
 brew cask install font-roboto-condensed
 brew cask install font-roboto-mono
@@ -124,11 +95,6 @@ brew cask install font-roboto-slab
 brew cask install font-source-code-pro
 brew cask install font-source-sans-pro
 brew cask install font-source-serif-pro
-brew cask install font-work-sans
-brew cask install font-karla
-brew cask install font-noto-sans
-brew cask install font-arimo
-brew cask install font-hind
 
 echo "Installing Quick Look Utilities..."
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json quicklook-csv
